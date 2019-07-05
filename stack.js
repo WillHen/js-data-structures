6416c328f19c80d88de01812f3013a6b78d3a403
William <will.henshaw@eatstreet.com>
2019-07-05 14:12:02 -0500

    add stack implementation

---
+++ stack.js
@@ -0,0 +1,27 @@
+class Stack {
+	constructor(item) {
+		this.stack = []
+		this.count = 0;
+
+	}
+
+	push(item) {
+		this.stack.unshift(item);
+		this.count++;
+
+	}
+
+	pop(item) {
+		if (this.count === 0) {
+			return undefined;
+		}
+		let poppedElement = this.stack.pop();
+		this.count--;
+		console.log(poppedElement)
+		return poppedElement;
+	}
+
+	size() {
+		return this.count;
+	}
+}
