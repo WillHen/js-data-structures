ba8d39854d276019e10e15cefdd0ea368969e128
William <will.henshaw@eatstreet.com>
2019-07-06 11:31:07 -0500

    make graph a class

--- graph.js
+++ graph.js
@@ -1,35 +1,33 @@
-function Graph() {
-	this.edges = {};
-	this.nodes = [];
+class Graph {
+	constructor() {
+		this.edges = {};
+		this.nodes = [];
+	}
+
+	addNode(node) {
+		this.nodes.push(node);
+		this.edges[node] = [];
+	}
+
+	addEdge(startNode, endNode) {
+		this.edges[startNode].push({ node: endNode });
+		this.edges[endNode].push({ node: startNode });
+	}
+
+	contains(node) {
+		return !!this.edges[node];
+	}
+
+	display() {
+
+		let graph = '';
+		this.nodes.forEach(node => {
+			graph +=
+				node +
+				'->' +
+				this.edges[node].map(n => n.node).join(', ') +
+				'\n';
+		});
+		console.log(graph);
+	}
 }
-
-Graph.prototype.addNode = function(node) {
-	this.nodes.push(node);
-	this.edges[node] = [];
-};
-
-Graph.prototype.contains = function(node) {
-	return !!this.edges[node];
-};
-
-Graph.prototype.addEdge = function(startNode, endNode) {
-	this.edges[startNode].push({ node: endNode });
-	this.edges[endNode].push({ node: startNode });
-};
-
-Graph.prototype.display = function() {
-	var graph = "";
-
-	this.nodes.forEach(node => {
-		graph +=
-			node +
-			"->" +
-			this.edges[node]
-				.map(function(n) {
-					n.node;
-				})
-				.join(", ") +
-			"\n";
-	});
-	console.log(graph);
-};
